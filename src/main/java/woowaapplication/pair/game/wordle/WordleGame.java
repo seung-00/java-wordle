8142c3e3c493ef34259f71a01c865e6ea9ab18ba
SeungYoung Oh <seung-00@naver.com>
2023-04-01 18:08:47 +0900

    refactor:  출력하는 부분들을 적합한 곳으로 위임했습니다

--- src/main/java/woowaapplication/pair/game/wordle/WordleGame.java
+++ src/main/java/woowaapplication/pair/game/wordle/WordleGame.java
@@ -27,12 +27,12 @@ public class WordleGame {
             try {
                 run(sc);
             } catch (InvalidInputKeywordException e) {
-                System.out.println("올바른 입력이 아닙니다. 다시 시도해주세요.");
+                System.out.println(e.getMessage());
             } catch (InvalidAnswerKeywordException e) {
-                System.out.println("오늘의 정답 키워드가 유효하지 않습니다. 관리자에게 문의해주세요.");
+                System.out.println(e.getMessage());
                 break;
             } catch (ReadFileException e) {
-                System.out.println("파일을 읽는데 실패했습니다. 관리자에게 문의해주세요.");
+                System.out.println(e.getMessage());
                 break;
             }
         }
@@ -52,13 +52,12 @@ public class WordleGame {
     private Scanner ready() {
         // 인트로 출력
         Scanner sc = new Scanner(System.in);
-        System.out.println("WORDLE을 6번 만에 맞춰 보세요.");
-        System.out.println("시도의 결과는 타일의 색 변화로 나타납니다.");
+        WordleGameUI.printReady();
 
         return sc;
     }
 
     private void terminate() {
-        System.out.println("게임이 종료되었습니다.");
+        WordleGameUI.printTerminate();
     }
 }
--- src/main/java/woowaapplication/pair/game/wordle/WordleGameUI.java
+++ src/main/java/woowaapplication/pair/game/wordle/WordleGameUI.java
@@ -10,6 +10,14 @@ public class WordleGameUI {
         this.wordleGameStorage = wordleGameStorage;
     }
 
+    public static void printReady() {
+        System.out.println("WORDLE을 6번 만에 맞춰 보세요.");
+        System.out.println("시도의 결과는 타일의 색 변화로 나타납니다.");
+    }
+
+    public static void printTerminate() {
+        System.out.println("게임이 종료되었습니다.");
+    }
 
     public void printResult(List<String[]> gameResult) {
         if (wordleGameStorage.isClear()) {
